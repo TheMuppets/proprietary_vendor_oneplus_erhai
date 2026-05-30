8da4df54ea5205ed1d39db5dbe394de0b60845ab
Bruno Martins <bgcngm@gmail.com>
2026-05-30 12:55:05 +0100

    erhai: De-blob oplus interfaces
    Change-Id: Ic353cb9678ebbc836b3790de451007466814d39d

--- erhai-vendor.mk
+++ erhai-vendor.mk
@@ -1227,8 +1227,6 @@ PRODUCT_PACKAGES += \
     libubifocus \
     libvideoml \
     sensors.qsh \
-    vendor.oplus.hardware.camera_rfi-V3-ndk \
-    vendor.oplus.hardware.cammidasservice-V1-ndk_vendor \
     vendor.qti.hardware.camera.aon-service-impl \
     vendor.qti.hardware.camera.offlinecamera-service-impl \
     com.qti.sensor.erhaifront \
@@ -1359,14 +1357,9 @@ PRODUCT_PACKAGES += \
     libwrapper_te \
     libyuvwrapper \
     libzlib \
-    vendor.oplus.hardware.camera.aon-V1-ndk_platform \
     vendor.oplus.hardware.camera.aon-service-impl \
-    vendor.oplus.hardware.camera_rfi-V1-ndk \
     vendor.oplus.hardware.camera_rfi-V1-service-impl \
-    vendor.oplus.hardware.cammidasservice-V1-ndk \
-    vendor.oplus.hardware.pogo_keyboard-V1-ndk_platform \
     vendor.oplus.hardware.sendextcamcmd-V1-service-impl \
-    vendor.oplus.hardware.sendextcamcmd-V2-ndk \
     vendor.qti.camera.aon-impl.xml \
     vendor.qti.camera.offlinecamera-impl.xml \
     vendor.qti.camera.provider.xml \
